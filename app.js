30010fe79b49f6f8e5e17a112b35ad68973bace0
Alexander Troshin <alexander@troshin.eu>
2015-02-28 23:54:10 +0100

    Added Edge to the user-agent check
    Edge is Internet Explorer 11 in the Windows 10 Technical Preview

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use(function(req, res, next) {
 app.use(function(req, res, next) {
     res.locals.is_ie = false;
     var ua = req.headers['user-agent'];
-    if(ua.search('Trident') > 0 || ua.search('MSIE') > 0) {
+    if(ua.search('Trident') > 0 || ua.search('MSIE') > 0 || ua.search('Edge') > 0) {
         res.locals.is_ie = true;
     }
     res.locals.current_url = req.path; // This is the important line
